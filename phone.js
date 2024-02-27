2d8175ce6068dc3feef1b4493c2cec952967bcd0
MdShuvo18 <mdks33881@gmail.com>
2024-02-27 21:54:24 +0600

    create and append div and child and also get photos of phone

--- phone.js
+++ phone.js
@@ -1,7 +1,40 @@
-const loadPhone = async() => {
+const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    console.log(data);
+    // console.log(data)
+    const phones = data.data;
+    // console.log(phones)
+    displayPhones(phones)
 }
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        // console.log(phone)
+        const createPhoneDiv = document.createElement('div');
+        createPhoneDiv.classList = `card p-4 bg-gray-100 shadow-xl`;
+        createPhoneDiv.innerHTML = `
+        <figure><img src="${phone.image}"
+        alt="Shoes" />
+        </figure>
+
+        <div class="card-body">
+
+        <h2 class="card-title">${phone.phone_name}</h2>
+
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+
+        </div>  
+
+        `
+
+        phoneContainer.appendChild(createPhoneDiv)
+    });
+}
+
 loadPhone()
 
